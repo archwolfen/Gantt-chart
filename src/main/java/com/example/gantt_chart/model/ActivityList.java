123f462e5547e0787d9777f125bb916ad8e76e37
lad1slav <jay.k0lad@gmail.com>
2019-05-26 00:40:05 +0300

    class name and method were fixed

--- src/main/java/com/example/gantt_chart/model/ActivityList.java
+++ src/main/java/com/example/gantt_chart/model/ActivityList.java
@@ -1,12 +1,20 @@
 package com.example.gantt_chart.model;
 
 import com.example.gantt_chart.model.activity.Activity;
+import com.example.gantt_chart.model.activity.Dates;
 import com.google.gson.JsonObject;
 
 import java.util.ArrayList;
 
-public class Activities implements Convertible {
+public class ActivityList implements Convertible {
+
     private ArrayList<Activity> allActivities;
+
+
+    public Dates calculateCriticalPath() {
+        return null;
+    }
+
     public JsonObject toJson() {
         return null;
     }
--- src/main/java/com/example/gantt_chart/model/activity/Activity.java
+++ src/main/java/com/example/gantt_chart/model/activity/Activity.java
@@ -3,8 +3,6 @@ package com.example.gantt_chart.model.activity;
 import com.example.gantt_chart.model.Convertible;
 import com.google.gson.JsonObject;
 
-import java.util.ArrayList;
-
 public abstract class Activity implements Convertible {
     private Dates startFinal;
     private Progress progress;
