b49a512d8d734e471a8ffdf8f7e2961e5c9ce926
Kostianytn Synytsia <kostiantyn.synytsia@gmail.com>
2019-05-26 02:48:55 +0300

    Add executor and executor list classes

---
+++ src/main/java/com/example/gantt_chart/model/activity/Executor.java
@@ -0,0 +1,49 @@
+package com.example.gantt_chart.model.activity;
+
+import com.example.gantt_chart.model.Convertible;
+import com.google.gson.JsonObject;
+
+public class Executor implements Convertible {
+    private String name;
+    private String surname;
+    private String photoUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public Executor(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Executor(String name, String surname, String photoUrl) {
+        this.name = name;
+        this.surname = surname;
+        this.photoUrl = photoUrl;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public JsonObject toJson() {
+        return null;
+    }
+}
---
+++ src/main/java/com/example/gantt_chart/model/activity/ExecutorList.java
@@ -0,0 +1,18 @@
+package com.example.gantt_chart.model.activity;
+
+import com.example.gantt_chart.model.Convertible;
+import com.google.gson.JsonObject;
+
+import java.util.ArrayList;
+
+public class ExecutorList implements Convertible {
+    private ArrayList<Executor> executors = new ArrayList<Executor>();
+
+    public void addExecutor() {
+
+    }
+
+    public JsonObject toJson() {
+        return null;
+    }
+}
