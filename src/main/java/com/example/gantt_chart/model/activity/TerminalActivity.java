71c732184defc25958c841b027720de48161732b
lad1slav <jay.k0lad@gmail.com>
2019-05-26 01:02:59 +0300

    TerminalActivity class was created

---
+++ src/main/java/com/example/gantt_chart/model/activity/TerminalActivity.java
@@ -0,0 +1,13 @@
+package com.example.gantt_chart.model.activity;
+
+import com.google.gson.JsonObject;
+
+public class TerminalActivity extends Activity {
+
+
+
+    @Override
+    public JsonObject toJson() {
+        return super.toJson();
+    }
+}
