648b03d89a4920f7d6f3b613c64a91b0ddf045e1
Kostianytn Synytsia <kostiantyn.synytsia@gmail.com>
2019-05-27 00:08:06 +0300

    Implemented toJson in SubActivities

--- src/main/java/com/example/gantt_chart/model/activity/SubActivities.java
+++ src/main/java/com/example/gantt_chart/model/activity/SubActivities.java
@@ -1,12 +1,16 @@
 package com.example.gantt_chart.model.activity;
 
 import com.example.gantt_chart.model.Convertible;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 
 import java.util.ArrayList;
 
 public class SubActivities implements Convertible {
     private ArrayList<TerminalActivity> activities = new ArrayList<TerminalActivity>();
+
+    public SubActivities() {
+    }
 
     public void addActivity(TerminalActivity activity) {
         activities.add(activity);
@@ -17,6 +21,10 @@ public class SubActivities implements Convertible {
     }
 
     public JsonElement toJson() {
-        return null;
+        JsonArray jsonArray = new JsonArray();
+        for (TerminalActivity activity : activities) {
+            jsonArray.add(activity.toJson());
+        }
+        return jsonArray;
     }
 }
