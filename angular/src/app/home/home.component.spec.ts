50f7b342542c64095939a691c50c541e63a22e9b
Arch <arch@Arch.localdomain>
2019-05-28 22:22:23 +0300

    Adding subactivities functionality added

--- angular/src/app/home/home.component.spec.ts
+++ angular/src/app/home/home.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ListActivitiesComponent } from './list-activities.component';
+import { HomeComponent } from './home.component';
 
-describe('ListActivitiesComponent', () => {
-  let component: ListActivitiesComponent;
-  let fixture: ComponentFixture<ListActivitiesComponent>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ListActivitiesComponent ]
+      declarations: [ HomeComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ListActivitiesComponent);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
