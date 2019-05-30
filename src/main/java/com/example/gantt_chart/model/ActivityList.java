145fe14c298637e6e18fe3a5df8081c457a7bf14
Kostianytn Synytsia <kostiantyn.synytsia@gmail.com>
2019-05-30 19:41:02 +0300

    Implement toJson in ActivityList

--- src/main/java/com/example/gantt_chart/model/ActivityList.java
+++ src/main/java/com/example/gantt_chart/model/ActivityList.java
@@ -1,19 +1,32 @@
 package com.example.gantt_chart.model;
 
+import com.example.gantt_chart.exceptions.DatesException;
 import com.example.gantt_chart.model.activity.Dates;
+import com.example.gantt_chart.model.activity.ID;
+import com.example.gantt_chart.model.activity.IDList;
 import com.example.gantt_chart.model.activity.TerminalActivity;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
+import java.util.Date;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class ActivityList extends ArrayList<TerminalActivity> implements Convertible {
-    private Dates projectTermins;
-
-    public Dates calculateCriticalPath() {
-        return null;
-    }
 
     public JsonElement toJson() {
-        return null;
+        JsonObject jsonObject = new JsonObject();
+
+        JsonArray jsonArray = new JsonArray();
+        for (TerminalActivity activity : this) {
+            jsonArray.add(activity.toJson());
+        }
+        jsonObject.add("activities", jsonArray);
+
+        CriticalPath path = new CriticalPath(this);
+        jsonObject.add("critical", path.toJson());
+        return jsonObject;
     }
 }
