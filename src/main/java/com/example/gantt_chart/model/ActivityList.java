b65490dd53c4b36f02ab23bc1bde59d6359741a1
lad1slav <jay.k0lad@gmail.com>
2019-06-01 21:20:20 +0300

    Add checking date bounds for each summary activity

--- src/main/java/com/example/gantt_chart/model/ActivityList.java
+++ src/main/java/com/example/gantt_chart/model/ActivityList.java
@@ -1,7 +1,9 @@
 package com.example.gantt_chart.model;
 
+import com.example.gantt_chart.exceptions.DatesException;
 import com.example.gantt_chart.model.activity.SubActivities;
 import com.example.gantt_chart.model.activity.SummaryActivity;
+import com.example.gantt_chart.model.activity.TerminalActivity;
 import com.google.gson.JsonElement;
 
 public class ActivityList extends SubActivities {
@@ -9,13 +11,20 @@ public class ActivityList extends SubActivities {
     @Override
     public void sort() {
         this.forEach(activity -> {
-            if(activity instanceof SummaryActivity)
-            {
+            if (activity instanceof SummaryActivity) {
                 ((SummaryActivity) activity).getSubactivities().sort();
             }
         });
 
         super.sort();
+    }
+
+    public void checkDateBounds() throws DatesException {
+        for (TerminalActivity activity : this) {
+            if (activity instanceof SummaryActivity) {
+                ((SummaryActivity) activity).checkDateBounds();
+            }
+        }
     }
 
     @Override
