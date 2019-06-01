66c5dc77eab128e8cfb62937b5904bed13531631
lad1slav <jay.k0lad@gmail.com>
2019-06-01 18:41:14 +0300

    sort() was added

--- src/main/java/com/example/gantt_chart/model/ActivityList.java
+++ src/main/java/com/example/gantt_chart/model/ActivityList.java
@@ -1,9 +1,22 @@
 package com.example.gantt_chart.model;
 
 import com.example.gantt_chart.model.activity.SubActivities;
+import com.example.gantt_chart.model.activity.SummaryActivity;
 import com.google.gson.JsonElement;
 
 public class ActivityList extends SubActivities {
+
+    @Override
+    public void sort() {
+        this.forEach(activity -> {
+            if(activity instanceof SummaryActivity)
+            {
+                ((SummaryActivity) activity).getSubactivities().sort();
+            }
+        });
+
+        super.sort();
+    }
 
     @Override
     public JsonElement toJson() {
